a73ee797a2c8b48370902c7529ff9c192c2c830d
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-02-23 20:29:11 +0200

    Add requester as abstract fetching functionality

---
+++ client/src/services/requester.js
@@ -0,0 +1,47 @@
+const host = 'http://localhost:3030';
+
+const request = async (method, url, data) => {
+    const option = {
+        method,
+        headers: {}
+    }
+
+    const token = localStorage.getItem('accessToken');
+
+    if (token) {
+        option.headers['X-Authorization'] = token;
+    }
+
+    if (data !== undefined) {
+        option.headers['Content-Type'] = 'application/json';
+        option.body = JSON.stringify(data);
+    }
+
+    try {
+        const response = await fetch(host + url, option);
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+
+        if (response.status === 204) {
+            return response;
+        }
+
+        return response.json();
+    } catch (error) {
+        throw error;
+    }
+}
+
+const get = (url) => request('GET', url);
+const post = (url, data) => request('POST', url, data);
+const put = (url, data) => request('PUT', url, data);
+const del = (url) => request('DELETE', url);
+
+export default {
+    get,
+    post,
+    put,
+    del
+}
