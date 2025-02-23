3ff222da44822f8448cb1b95c4fd4f2f2a85bd6a
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-02-23 22:35:39 +0200

    Refactor Pagination page

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -1,32 +1,27 @@
-import { useEffect, useState } from "react";
-
 import styles from './Pagination.module.css';
 
 export default function Pagination({
-    length,
-    itemsPerPage,
-    changeCurrentPageHandler
+    currentPage,
+    hasNextPage,
+    handlePageChange
 }) {
-    const [pages, setPages] = useState([]);
-
-    useEffect(() => {
-        const pageCount = Math.ceil(length / itemsPerPage);
-        const pageNumbers = [];
-
-        for (let i = 1; i <= pageCount; i++) {
-            pageNumbers.push(i);
-        }
-
-        setPages(pageNumbers);
-    }, [length]);
-
-    const pageClickHandler = (page) => {
-        changeCurrentPageHandler(page)
-    }
-
     return (
-        <div className={styles.pagination}>
-            {pages.map((el, i) => <button className='button' key={el} onClick={() => pageClickHandler(el)}>{el}</button>)}
+        <div className={styles.container}>
+            <button
+                onClick={() => handlePageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+            >
+                Prev
+            </button>
+
+            <span>Page {currentPage}</span>
+
+            <button
+                onClick={() => handlePageChange(currentPage + 1)}
+                disabled={!hasNextPage}
+            >
+                Next
+            </button>
         </div>
     );
 };
