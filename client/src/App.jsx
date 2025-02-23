e2f44ae871321763d5b7a141dbc787ae31204d2a
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-02-23 18:16:37 +0200

    Add 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/header/Header.jsx"
 import { PATHS } from "./constants.js"
 import Home from "./components/home/Home.jsx"
 import Footer from "./components/footer/Footer.jsx"
+import PageNotFound from "./components/page-not-found/PageNotFound.jsx"
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
 
             <Routes>
                 <Route path={PATHS.Home} element={<Home />} />
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
 
             <Footer />
---
+++ client/src/components/page-not-found/PageNotFound.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import styles from './PageNotFound.module.css';
+import { PATHS } from "../../constants.js";
+
+export default function PageNotFound() {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>404</h1>
+            <p className={styles.message}>
+                Oops! Looks like this page doesn't exist... 😔
+            </p>
+            <p className={styles.description}>
+                The page you're looking for might be lost in the sound. But don't
+                worry, the rock goes on!
+            </p>
+            <Link to={PATHS.Home} className={styles.button}>
+                Go Back to Rock Home
+            </Link>
+
+        </div>
+    );
+};
