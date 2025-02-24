dda46e4c16cdaacf2ba560de93ef5715a6eccf65
atanasbondzhakov <atanas.b@yahoo.com>
2025-02-24 13:03:51 +0200

    Update Header to show navigation dynamically

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+
 import styles from './Header.module.css';
 import { PATHS } from '../../constants.js';
+import AuthContext from '../../contexts/authContext.jsx';
 
 export default function Header() {
+    const { isAuthenticated } = useContext(AuthContext);
+
     return (
         <div className={styles.container}>
             <Link to={PATHS.Home}><img src='/images/logo.png' alt='Logo' className={styles.logo} /></Link>
@@ -11,17 +16,19 @@ export default function Header() {
                 <li><Link to={PATHS.Home}>Home</Link></li>
                 <li><Link to={PATHS.Albums}>Albums</Link></li>
 
+                {isAuthenticated
+                    ?
                     <div className={styles.auth}>
                         <li><Link to={PATHS.AddAlbum}>Add Album</Link></li>
                         <li><Link to="#">Logout</Link></li>
                     </div>
-
+                    :
                     <div className={styles.guest}>
                         <li><Link to={PATHS.Login}>Login</Link></li>
                         <li><Link to={PATHS.Register}>Register</Link></li>
                         <li><Link to={PATHS.About}>About</Link></li>
                     </div>
-
+                }
             </ul>
         </div >
     );
