01576f4fc8c02f4787f2412145b9e80d14494c2d
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-02-24 22:41:38 +0200

    Update useForm hook to work with Yup validation

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
-export default function useForm(initialValues, submitHandler) {
+export default function useForm(initialValues, submitHandler, validationSchema) {
     const [formValues, setFormValues] = useState(initialValues);
+    const [errors, setErrors] = useState({});
 
     const onChange = (e) => {
         setFormValues(state => ({ ...state, [e.target.name]: e.target.value }));
@@ -10,14 +11,27 @@ export default function useForm(initialValues, submitHandler) {
     const onSubmit = async (e) => {
         e.preventDefault();
 
-        await submitHandler(formValues);
+        try {
+            await validationSchema.validate(formValues, { abortEarly: false });
+            await submitHandler(formValues);
 
-        setFormValues(initialValues);
-    }
+            setErrors({});
+            setFormValues(initialValues);
+        } catch (err) {
+            const validationErrors = {};
+            
+            err.inner.forEach(error => {              
+                validationErrors[error.path] = error.message;
+            });
+
+            setErrors(validationErrors);
+        }
+    };
 
     return {
         formValues,
+        errors,
         onChange,
         onSubmit
-    };
+    }
 }
