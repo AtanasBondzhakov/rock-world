b3928faf1f7a02f6a6b072e8336e0e271aae2fa1
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-03-01 00:35:18 +0200

    Update footer styles and links

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -14,21 +14,21 @@ export default function Footer() {
     return (
         <div className={styles.container}>
             <ul className={styles.contactInfo}>
-                <li><MdEmail /> a******b@yahoo.com</li>
+                <li><MdEmail /> rock-world@yahoo.com</li>
                 <li><FaPhone /> 0888-444-333</li>
                 <li><FaLocationDot /> Springfield, XY 12345</li>
             </ul>
             <div className={styles.infoContainer}>
                 <ul className={styles.quickLinks}>
-                    <li><Link to={PATHS.Home}>Home</Link></li>
                     <li><Link to={PATHS.About}>About</Link></li>
                     <li><Link to={PATHS.Contacts}>Contacts</Link></li>
                     <li><Link to={PATHS.Albums}>Albums</Link></li>
+                    <li><Link to={PATHS.Register}>Register</Link></li>
                 </ul>
                 <div className={styles.socialMedia}>
-                    <Link to="#" target="_blank"><FaGithub /></Link>
-                    <Link to="#" target="_blank"><FaLinkedin /></Link>
-                    <Link to="#" target="_blank"><FaFacebook /></Link>
+                    <Link to="https://github.com/AtanasBondzhakov" target="_blank"><FaGithub /></Link>
+                    <Link to="https://www.linkedin.com/in/atanas-bondzhakov-85a4ab2b5" target="_blank"><FaLinkedin /></Link>
+                    <Link to="https://www.facebook.com/justbones.wth1/" target="_blank"><FaFacebook /></Link>
                 </div>
                 <div className={styles.copyright}>
                     <p>&copy; 2025 Rock World. All rights reserved.</p>
