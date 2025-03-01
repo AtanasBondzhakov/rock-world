790714bccb4cc2c534fa01288bf02485436f28aa
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-03-01 23:36:43 +0200

    Update header styles and improve responsiveness

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
+import { RxHamburgerMenu } from "react-icons/rx";
 
 import styles from './Header.module.css';
 import { PATHS } from '../../constants.js';
@@ -10,10 +11,15 @@ export default function Header() {
     const location = useLocation();
 
     const [active, setActive] = useState(PATHS.Home);
+    const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
         setActive(location.pathname);
     }, [location.pathname]);
+
+    const handleMobileMenu = () => {
+        setIsMobile(state => !state);
+    }
 
     return (
         <div className={styles.container}>
@@ -97,6 +103,89 @@ export default function Header() {
                     </div>
                 }
             </ul>
+
+            <div className={styles.mobile}>
+
+                <div className={styles.hamburger}>
+                    <RxHamburgerMenu className={styles.icon} onClick={handleMobileMenu} />
+                </div>
+
+                <ul className={`${styles.mobileMenu} ${isMobile ? styles.mobileActive : ''}`}>
+                    <li>
+                        <Link
+                            to={PATHS.Home}
+                            className={active === PATHS.Home ? styles.active : ''}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to={PATHS.Albums}
+                            className={active === PATHS.Albums ? styles.active : ''}
+                        >
+                            Albums
+                        </Link>
+                    </li>
+
+                    {isAuthenticated
+                        ?
+                        <div className={styles.auth}>
+                            <li>
+                                <Link
+                                    to={PATHS.AddAlbum}
+                                    className={active === PATHS.AddAlbum ? styles.active : ''}
+                                >
+                                    Add
+                                    Album</Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={PATHS.Profile}
+                                    className={active === PATHS.Profile ? styles.active : ''}
+                                >
+                                    Profile
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={PATHS.Logout}
+                                    className={active === PATHS.Logout ? styles.active : ''}
+                                >
+                                    Logout
+                                </Link>
+                            </li>
+                        </div>
+                        :
+                        <div className={styles.guest}>
+                            <li>
+                                <Link
+                                    to={PATHS.Login}
+                                    className={active === PATHS.Login ? styles.active : ''}
+                                >
+                                    Login
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={PATHS.Register}
+                                    className={active === PATHS.Register ? styles.active : ''}
+                                >
+                                    Register
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to={PATHS.About}
+                                    className={active === PATHS.About ? styles.active : ''}
+                                >
+                                    About
+                                </Link>
+                            </li>
+                        </div>
+                    }
+                </ul>
+            </div>
         </div >
     );
 };
