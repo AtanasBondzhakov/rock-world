05911348354993cfeb2e671b819b75b1c80d5778
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-03-20 01:01:30 +0200

    Add useGetAllAlbums custom hook

--- client/src/api/albumsApi.js
+++ client/src/api/albumsApi.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import requester from "../services/requester.js";
 
 const BASE_PATH = '/data/albums';
@@ -7,5 +8,40 @@ export const useCreateAlbum = () => {
 
     return {
         createAlbum
-    }
+    };
+};
+
+export const useGetAllAlbums = (offset, pageSize) => {
+    const [albums, setAlbums] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+    const [hasNextPage, setHasNextPage] = useState(true);
+
+    const query = new URLSearchParams({
+        sortBy: '_createdOn desc',
+        offset,
+        pageSize: pageSize + 1
+    });
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const result = await requester.get(`${BASE_PATH}?${query}`);
+
+                setAlbums(result.slice(0, pageSize));
+                setHasNextPage(result.length > pageSize)
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, [offset]);
+
+    return {
+        albums,
+        loading,
+        error,
+        hasNextPage
+    };
 };
--- client/src/components/albums/all-albums/AllAlbums.jsx
+++ client/src/components/albums/all-albums/AllAlbums.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import styles from './AllAlbums.module.css';
-import albumService from '../../../services/albumService.js';
+import { useGetAllAlbums } from '../../../api/albumsApi.js';
 
 import Spinner from '../../spinner/Spinner.jsx';
 import AlbumItem from './album-item/AlbumItem.jsx';
@@ -10,29 +10,12 @@ import ScrollToTop from '../../scroll-to-top/ScrollToTop.jsx';
 import ErrorMessage from '../../error-message/ErrorMessage.jsx';
 
 export default function AllAlbums() {
-    const [albumsList, setAlbumsList] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [pageSize] = useState(10);
+    const [pageSize, setPageSize] = useState(10);
     const [currentPage, setCurrentPage] = useState(1);
-    const [hasNextPage, setHasNextPage] = useState(true);
-    const [error, setError] = useState('');
 
-    useEffect(() => {
-        (async () => {
-            const offset = (currentPage - 1) * pageSize;
+    const offset = (currentPage - 1) * pageSize;
 
-            try {
-                const result = await albumService.getAll(offset, pageSize + 1);
-
-                setAlbumsList(result.slice(0, pageSize));
-                setHasNextPage(result.length > pageSize);
-            } catch (err) {
-                setError(err.message);
-            } finally {
-                setLoading(false);
-            }
-        })();
-    }, [currentPage]);
+    const { albums, loading, error, hasNextPage } = useGetAllAlbums(offset, pageSize);
 
     const handlePageChange = (page) => {
         setCurrentPage(page);
@@ -45,11 +28,11 @@ export default function AllAlbums() {
             <div className={styles.wrapper}>
                 {loading && <Spinner />}
 
-                {!loading && albumsList.length > 0 && (
+                {!loading && albums.length > 0 && (
                     <>
                         <h2>Album Collection</h2>
                         <div className={styles.container}>
-                            {albumsList.map(album => <AlbumItem key={album._id} {...album} />)}
+                            {albums.map(album => <AlbumItem key={album._id} {...album} />)}
                         </div>
 
                         <Pagination
@@ -60,11 +43,11 @@ export default function AllAlbums() {
                     </>
                 )}
 
-                {!loading && albumsList.length === 0 && !error && (
+                {!loading && albums.length === 0 && !error && (
                     <h2>There is no albums yet.</h2>
                 )}
 
-                {!loading && albumsList.length === 0 && error && (
+                {!loading && albums.length === 0 && error && (
                     <ErrorMessage message={error} />
                 )}
             </div>
