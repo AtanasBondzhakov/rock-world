7a15c71ec3491eb005807b394cbb98681686a67d
atanasbondzhakov <atanas.b@yahoo.com>
2025-03-21 15:23:29 +0200

    Refactor details page to use custom hook

--- client/src/components/albums/details-album/DetailsAlbum.jsx
+++ client/src/components/albums/details-album/DetailsAlbum.jsx
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom';
 
 import styles from './DetailsAlbum.module.css';
 import AuthContext from '../../../contexts/authContext.jsx';
-import commentService from '../../../services/commentService.js';
+import { useGetOneAlbum } from '../../../api/albumsApi.js';
+import { useCreateComment, useGetComments } from '../../../api/commentsApi.js';
 
 import DetailsAlbumItem from './details-album-item/DetailsAlbumItem.jsx';
 import Spinner from '../../spinner/Spinner.jsx';
@@ -12,56 +13,25 @@ import CommentAlbumItem from '../comment-album/comment-album-item/CommentAlbumIt
 import Pagination from '../../pagination/Pagination.jsx';
 import ErrorMessage from '../../error-message/ErrorMessage.jsx';
 import ScrollToTop from '../../scroll-to-top/ScrollToTop.jsx';
-import { useGetOneAlbum } from '../../../api/albumsApi.js';
-import { useCreateComment } from '../../../api/commentsApi.js';
 
 export default function DetailsAlbum() {
-    // const [album, setAlbum] = useState({});
-    // const [loading, setLoading] = useState(true);
-    const [comments, setComments] = useState([]);
     const [pageSize] = useState(5);
     const [currentPage, setCurrentPage] = useState(1);
-    const [hasNextPage, setHasNextPage] = useState(true);
-    const [commentError, setCommentError] = useState('');
 
     const { albumId } = useParams();
-    const { userId, isAuthenticated, email } = useContext(AuthContext);
+    const { userId, isAuthenticated } = useContext(AuthContext);
 
     const { album, loading, error } = useGetOneAlbum(albumId);
     const { createComment } = useCreateComment();
 
+    const offset = (currentPage - 1) * pageSize;
+
+    const { comments, error: commentsError, hasNextPage } = useGetComments(offset, pageSize, albumId);
+
     const isOwner = userId === album._ownerId;
 
-    const handleGetComments = async () => {
-        const offset = (currentPage - 1) * pageSize;
-
-        try {
-            const resultComments = await commentService.getAll(offset, pageSize + 1, albumId);
-
-            setHasNextPage(resultComments.length > pageSize);
-            setComments(resultComments.slice(0, pageSize));
-        } catch (err) {
-            setError(err.message);
-        }
-    };
-
-    // useEffect(() => {
-    //     (async () => {
-    //         try {
-    //             const resultAlbum = await albumService.getOne(albumId);
-
-    //             handleGetComments();
-
-    //             setAlbum(resultAlbum);
-    //         } catch (err) {
-    //             setError(err.message);
-    //         } finally {
-    //             setLoading(false);
-    //         }
-    //     })();
-    // }, [albumId, currentPage]);
-
-    //TODO fix comments not load on mount
+    //TODO show comments when new is added
+    //TODO stick errors in one state and show conditionally
 
     const handleAddComment = async (values) => {
         try {
@@ -69,7 +39,7 @@ export default function DetailsAlbum() {
 
             // handleGetComments();
         } catch (err) {
-            setCommentError(err.message);
+            // setCommentError(err.message);
         }
     };
 
