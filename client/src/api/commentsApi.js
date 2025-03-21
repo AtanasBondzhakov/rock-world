10928fc5e6f8d602514ec06c2e38f69d971a094b
atanasbondzhakov <atanas.b@yahoo.com>
2025-03-21 15:22:59 +0200

    Add useGetComments custom hook

--- client/src/api/commentsApi.js
+++ client/src/api/commentsApi.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import requester from "../services/requester.js";
 
 const BASE_URL = '/data/comments';
@@ -7,5 +8,38 @@ export const useCreateComment = () => {
 
     return {
         createComment
+    }
+};
+
+export const useGetComments = (offset, pageSize, albumId) => {
+    const [comments, setComments] = useState([]);
+    const [error, setError] = useState('');
+    const [hasNextPage, setHasNextPage] = useState(true);
+
+    const query = new URLSearchParams({
+        sortBy: '_createdOn desc',
+        offset,
+        pageSize: pageSize + 1,
+        where: `albumId="${albumId}"`,
+        load: `author=_ownerId:users`
+    });
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const result = await requester.get(`${BASE_URL}?${query}`);
+
+                setComments(result.slice(0, pageSize));
+                setHasNextPage(result.length > pageSize);
+            } catch (err) {
+                setError(err.message);
+            }
+        })();
+    }, [albumId, offset]);
+
+    return {
+        comments,
+        error,
+        hasNextPage
     }
 };
--- client/src/components/albums/comment-album/comment-album-item/CommentAlbumItem.jsx
+++ client/src/components/albums/comment-album/comment-album-item/CommentAlbumItem.jsx
@@ -2,14 +2,14 @@ import { dateFormatter } from '../../../../utils/dateUtil';
 import styles from './CommentAlbumItem.module.css';
 
 export default function CommentAlbumItem({
-    email,
+    author,
     _createdOn,
     comment
 }) {
 
     return (
         <div className={styles.container}>
-            <span>{email} - {dateFormatter(_createdOn)}</span>
+            <span>{author.email} - {dateFormatter(_createdOn)}</span>
             <p>{comment}</p>
         </div>
     );
