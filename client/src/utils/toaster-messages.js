5e007e10dc2c2993d69296ae7be21f10211433ea
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-03-22 23:52:34 +0200

    Update toaster message for small screens

--- client/src/utils/toaster-messages.js
+++ client/src/utils/toaster-messages.js
@@ -1,6 +1,8 @@
 import { toast } from "react-toastify";
 
 export const toasterSuccess = (message) => {
+    const isSmallScreen = window.innerWidth <= 768;
+
     toast.success(message, {
         position: 'bottom-right',
         autoClose: 3000,
@@ -9,5 +11,16 @@ export const toasterSuccess = (message) => {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
+        style: isSmallScreen
+            ? {
+                width: '200px',
+                height: '50px',
+                fontSize: '0.75em'
+            }
+            : {
+                width: '320px',
+                minHeight: '64px',
+                fontSize: '1em'
+            }
     });
 }
