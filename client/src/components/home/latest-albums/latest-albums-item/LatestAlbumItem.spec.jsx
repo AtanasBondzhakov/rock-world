fb81f0c7df6b5f4d0a7e78ced4ae67b5e00da557
Atanas Bondzhakov <atanas.b@yahoo.com>
2025-04-01 21:05:49 +0300

    Create Latests album item tests

---
+++ client/src/components/home/latest-albums/latest-albums-item/LatestAlbumItem.spec.jsx
@@ -0,0 +1,33 @@
+import { it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import LatestAlbumsItem from './LatestAlbumsItem.jsx';
+
+const mockAlbum = {
+    _id: "123",
+    imageUrl: "https://example.com/album.jpg"
+};
+
+it('Should display album image', () => {
+    render(
+        <MemoryRouter>
+            <LatestAlbumsItem {...mockAlbum} />
+        </MemoryRouter>
+    );
+
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveAttribute('src', mockAlbum.imageUrl);
+    expect(screen.getByRole('img')).toHaveAttribute('alt', 'image');
+});
+
+it('Should display correct album link', () => {
+    render(
+        <MemoryRouter>
+            <LatestAlbumsItem {...mockAlbum} />
+        </MemoryRouter>
+    );
+
+    const link = screen.getByRole("link");
+    expect(link).toHaveAttribute("href", `/albums/${mockAlbum._id}/details`);
+})
